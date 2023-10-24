35de6e7d877dccead4b7f4815f950cc14209ba80
Suman Mondal <145131739+Suman-Mondal007@users.noreply.github.com>
2023-10-24 21:25:25 +0530

    Create practice-1.c

---
+++ practice-1.c
@@ -0,0 +1,11 @@
+//Write  a program in c to check whether the line is straight or not.
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float y,m,x,c;
+    printf("enter the value of  y,m,x and c \n");
+    scanf("%f %f %f %f",&y,&m,&x,&c);
+    float mxc=m*x+c;
+    int sl=(y==mxc)?(y==mxc):(y==mxc);
+    printf("%d",sl);
+}

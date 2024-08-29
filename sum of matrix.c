17986e0da586b0e99db6214d0368ee29f73cb37d
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2024-08-29 17:38:01 +0530

    update

--- sum of matrix.c
+++ sum of matrix.c
@@ -1,20 +1,30 @@
-//sum of matrix
 #include<stdio.h>
 int main(){
-    int a[3][3],i,j,sum_row,sum_col;
-    printf("Enter the matrics:\n");
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
-            scanf("%d",&a[i][j]);
-        }
-    }
-    for(i=0;i<3;i++){
-        sum_row=sum_col=0;
-        for(j=0;j<3;j++){
-            sum_row=sum_row+a[i][j];
-            sum_col=sum_col+a[j][i];
-
-        }
-        printf("\n sum_row=%d,sum_col=%d",sum_row,sum_col);
-    }
-}
+    float a,b,sum,sub,div,mul;
+    char choice;
+    printf("Enter the value of a:\n");
+    scanf("%f",&a);
+    printf("Enter the value of b:\n");
+    scanf("%f",&b);
+    printf("Enter the choice (+,-,*,/):\n");
+    scanf(" %c",&choice);
+       switch (choice){
+           case '+':
+             sum=a+b;
+             printf("The sum of two number is:%f",sum);
+             break;
+           case'-':
+            sub=a-b;
+            printf("The sub of two number is:%f",sub);
+            break;
+           case '*':
+               mul=a*b;
+               printf("The mul of two number is:%f",mul);
+               break;
+           case'/':
+                   div = a / b;
+                   printf("The div of two number is:%f", div);
+              break;
+       }
+       printf("\n");
+  }
